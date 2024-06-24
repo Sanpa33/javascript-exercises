d4c02ee83357b8f6f86db6c7b1ee5d1ee65ccea2
sanpa <127140452+Sanpa33@users.noreply.github.com>
2024-06-24 04:02:22 -0300

    Add exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,29 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    // console.log("Original String");
+    // console.log(string);
+    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    // console.log("Split the string inside an array");
+    // const array = string.split("");
+    // console.log(array);
+    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+    // console.log("Invert the elements inside the array");
+    // array.reverse();
+    // console.log(array);
+    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    
+
+    // console.log("Join the complete array in a string");
+    // console.log(array.join(""));
+
+    return string.split("").reverse().join("");
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
